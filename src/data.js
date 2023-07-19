8c9ad3f6372747f441ac0d073131ab2dcbd14264
javad7721 <m.javad7721@gmail.com>
2023-07-19 18:25:38 +0330

    fix

--- src/data.js
+++ src/data.js
@@ -9,7 +9,7 @@ export const SongData=()=>{
             audio: "https://mp3.chillhop.com/serve.php/?mp3=21648",
             color: ["65383A","D0906D"],
             id: uuidv4(),
-            active: true
+            active: false
         },
         {
             name: "What If I Told You",
@@ -18,7 +18,67 @@ export const SongData=()=>{
             audio: "https://mp3.chillhop.com/serve.php/?mp3=21649",
             color: ["65383A","D0906D"],
             id: uuidv4(),
+            active: false
+        },
+        {
+            name: "Whale Call",
+            cover: "https://chillhop.com/wp-content/uploads/2020/07/385d44fc6335bf676b30be524bcdfd7d640716a4-1024x1024.jpg",
+            artist: "Saib",
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=8099",
+            color: ["65383A","D0906D"],
+            id: uuidv4(),
+            active: false
+            
+        },
+        {
+            name: "Build",
+            cover: "https://chillhop.com/wp-content/uploads/2020/07/f5170130ca62e18fb3928feb3d851f3f07f77e18-1024x1024.jpg",
+            artist: "Yasper, Louk",
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=7873",
+            color: ["65383A","D0906D"],
+            id: uuidv4(),
             active: true
-        }
+            
+        },
+        {
+            name: "Rockaway 5pm",
+            cover: "https://chillhop.com/wp-content/uploads/2020/07/8ead77b24ce86e871c31691dec6a5983b9533db1-1024x1024.jpg",
+            artist: "auv",
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=9157",
+            color: ["65383A","D0906D"],
+            id: uuidv4(),
+            active: false
+            
+        },
+        {
+            name: "Magenta Forever",
+            cover: "https://chillhop.com/wp-content/uploads/2020/10/23fdd99adc3e16abcb67b004ea3e748ebf433a49-1024x1024.jpg",
+            artist: "Aviino",
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=10458",
+            color: ["65383A","D0906D"],
+            id: uuidv4(),
+            active: false
+            
+        },
+        {
+            name: "Dojo",
+            cover: "https://chillhop.com/wp-content/uploads/2020/04/35317644d6db24c3ca144619d03f2805fa702472-1024x1024.jpg",
+            artist: "SwuM",
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=8002",
+            color: ["65383A","D0906D"],
+            id: uuidv4(),
+            active: false
+            
+        },
+        {
+            name: "Caffeine",
+            cover: "https://chillhop.com/wp-content/uploads/2020/07/858b533ba20ff95bf5b401089b195d1a8cb43870-1024x1024.jpg",
+            artist: "Felty",
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=9334",
+            color: ["65383A","D0906D"],
+            id: uuidv4(),
+            active: false
+            
+        },
     ]
 }
